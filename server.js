59e512f3cbd530589d4ad07222b2e1200e981d46
probin bhowmick <119671621+pro221@users.noreply.github.com>
2023-04-11 20:36:20 +0530

    new update1

--- server.js
+++ server.js
@@ -7,12 +7,17 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from 'url';
 
 // configure dotenv
 dotenv.config();
 
 //database
 connectDB();
+
+//esmodule fix
+const __filename= fileURLToPath(import.meta.url);
+const __dirname= path.dirname(__filename);
 
 const app = express();
 
